837bdc9212408cf1bcfcdb7599fa3217e86f0089
Andrea Lavista <andrea.lavista@studio.unibo.it>
2019-11-19 16:19:48 +0100

    Funzione esponenziale

--- parser.py
+++ parser.py
@@ -1,12 +1,17 @@
 import csv
 import os
 import glob
+import statistics
+
 with open('dataset.csv', mode='w') as dataset:
     
+    durate = []
+
     mapping = {"B":0,"W":1}
     for filename in glob.glob('games/*.txt'):
         games = open(filename, "r").read()
         turns = games.count("FINE: Stato:")
+        durate.append(turns)
         winner = -1 if games.count("Nero vince") else 1 if games.count("Bianco vince") else 0   
         dataset_writer = csv.writer(dataset,lineterminator = '\n', delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file = open(filename, "r")
@@ -27,10 +32,22 @@ with open('dataset.csv', mode='w') as dataset:
                     file.readline()
                     #True se la mossa è del bianco
                     state.append(int(file.readline()[0] == "B"))
-                    state.append(evaluation/turns*winner)
+                    treshold = 1
+                    state.append((evaluation/turns)**(turns/treshold)*winner)
+                    #state.append((evaluation/turns)*winner)
                     dataset_writer.writerow(state)
                 first = False
-               
-                    
-                    
+"""        
+    print("durata media:"+str(statistics.mean(durate)))              
+    print("max:"+str(max(durate)))
+    print("min:"+str(min(durate)))
+
+    n = 0
+    for i in range(len(durate)):
+        if durate[i] <= 26:
+            n+=1
+
+    print(str(n) + " su " + str(len(durate)))
+    #print(durate)
+"""
                     
